908bb77447ddecf2349540b7c9bfcecc658d8605
Suellen Castro <castrosuellenx@gmail.com>
2022-01-13 17:09:37 -0300

    inicial_code

---
+++ src/components/Counter/index.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react';
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <span>
+        <b>Count: </b>
+        {count}
+      </span>
+
+      <br />
+
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+    </div>
+  );
+}
